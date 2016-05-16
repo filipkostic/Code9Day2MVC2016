a9d2f31fb94225c3da4df8d0f37af489f1b8a060
Filip Kostić <Filip Kosti>
2016-05-16 23:13:46 +0200

    Added validations for [Country] model.

--- Code9MVC/Models/Country.cs
+++ Code9MVC/Models/Country.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Code9MVC.Models
@@ -8,12 +9,15 @@ namespace Code9MVC.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
 
+        [RegularExpression(@"^[a-zA-Z \-']+$", ErrorMessage = "Country name can contain only letters, space, minus or apostrophe.")]
         public string Name { get; set; }
 
+        [StringLength(2, MinimumLength = 2, ErrorMessage = "International code for a country name has two characters.")]
         public string ShortName { get; set; }
 
         public string Currency { get; set; }
 
+        [Range(0, double.MaxValue, ErrorMessage = "Country can't have a negative area.")]
         public double Area { get; set; }
 
         public virtual ICollection<City> Cities { get; set; }
